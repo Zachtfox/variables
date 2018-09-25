0c5a9aa2854e581833c7b340762591ba1cc06c9e
Zachtfox <31944870+Zachtfox@users.noreply.github.com>
2018-09-25 10:55:51 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+	byte b = 1;
+	short s = 2;
+	int i = 3;
+	long l = 888888;
+	float f = 3.14f;
+	double d = 2.25;
+	boolean boo = true;
+	char c = 'Z' ;
+
+        System.out.println( "Byte example: " + b);
+        System.out.println( "Short example: " + s);
+        System.out.println( "Int example: " + i);
+        System.out.println( "Long example: " + l);
+        System.out.println( "Float example: " + f);
+        System.out.println( "Double example: " + d);
+        System.out.println( "Boolean example: " + boo);
+        System.out.println( "Char example: " + c);
+
+
+    }
+}
